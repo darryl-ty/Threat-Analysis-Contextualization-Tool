e141f4e025b4c59f9e9c46cec00b8a131066fcc6
darryl-ty <darrylterrell921@gmail.com>
2023-05-14 23:57:01 -0500

    Setup window placement and prototype login screen.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/Main.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/Main.java
@@ -2,7 +2,9 @@ package com.tact.threatanalysiscontextualizationtool;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +14,14 @@ public class Main extends Application{
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("TACT - Threat Analysis and Contextualization Tool");
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setWidth(primScreenBounds.getWidth() - (primScreenBounds.getHeight()/ 2));
+        stage.setHeight(primScreenBounds.getHeight() - (primScreenBounds.getHeight()/ 4));
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {

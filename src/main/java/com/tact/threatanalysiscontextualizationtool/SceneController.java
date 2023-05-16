b6ab4368a29d10ffb2c8b75a3c7bf15888b33314
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2023-05-16 09:51:38 -0500

    Setup main page for file upload and url screening.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/SceneController.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/SceneController.java
@@ -1,7 +1,29 @@
 package com.tact.threatanalysiscontextualizationtool;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class SceneController {
+
+    public void switchToMainPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("main-page.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("TACT - Threat Analysis and Contextualization Tool");
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setWidth(primScreenBounds.getWidth() - (primScreenBounds.getHeight()/ 2));
+        stage.setHeight(primScreenBounds.getHeight() - (primScreenBounds.getHeight()/ 4));
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }

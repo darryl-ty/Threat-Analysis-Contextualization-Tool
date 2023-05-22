c9a8453195805eb9e42a93f17e9e726047541067
darryl-ty <darrylterrell921@gmail.com>
2023-05-21 23:52:40 -0400

    Update UI of main-page.fxml to add upload, search, and options button.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/Main.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/Main.java
@@ -1,9 +1,14 @@
 package com.tact.threatanalysiscontextualizationtool;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Scale;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -12,17 +17,19 @@ import java.io.IOException;
 public class Main extends Application{
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FXML/main-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("TACT - Threat Analysis and Contextualization Tool");
+        Scale scale = new Scale(1.2, 1.2);
+        scale.setPivotX(0);
+        scale.setPivotY(0);
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
         stage.setWidth(primScreenBounds.getWidth() - (primScreenBounds.getHeight()/ 2));
         stage.setHeight(primScreenBounds.getHeight() - (primScreenBounds.getHeight()/ 4));
-
+        scene.getRoot().getTransforms().setAll(scale);
 
         stage.setScene(scene);
         stage.show();
-
 
     }
 

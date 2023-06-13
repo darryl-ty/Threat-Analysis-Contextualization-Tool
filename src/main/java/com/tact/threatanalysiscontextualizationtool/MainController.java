aae020d5df994a746802f7655ad187e19bed1caf
darryl-ty <darrylterrell921@gmail.com>
2023-06-13 14:11:51 -0400

    Create model upload popup.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
@@ -1,11 +1,16 @@
 package com.tact.threatanalysiscontextualizationtool;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TitledPane;
 
 public class MainController {
 
-    public void uploadPopupWindow(ActionEvent event){
+    @FXML
+    private TitledPane uploadPopup;
 
+    public void uploadPopupWindow(ActionEvent event){
+        uploadPopup.setVisible(true);
     }
 
     public void settingsPopupWindow(ActionEvent event){

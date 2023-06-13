2c534bdf400198bae5ce2ba5d779c082d7277c2c
darryl-ty <darrylterrell921@gmail.com>
2023-06-13 14:17:57 -0400

    Implement greyout of background when user selects upload button.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
@@ -3,14 +3,18 @@ package com.tact.threatanalysiscontextualizationtool;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController {
 
     @FXML
     private TitledPane uploadPopup;
+    @FXML
+    private AnchorPane greyOut;
 
     public void uploadPopupWindow(ActionEvent event){
         uploadPopup.setVisible(true);
+        greyOut.setVisible(true);
     }
 
     public void settingsPopupWindow(ActionEvent event){

19aefe71cfbbfed340cf93721b7ba2649158b118
darryl-ty <darrylterrell921@gmail.com>
2023-06-12 20:22:44 -0400

    Begin setup of popup windows for upload button and settings button.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/MainController.java
@@ -1,4 +1,16 @@
 package com.tact.threatanalysiscontextualizationtool;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public void uploadPopupWindow(ActionEvent event) throws IOException{
+
+    }
+
+    public void settingsPopupWindow(ActionEvent event) throws IOException{
+
+    }
 }

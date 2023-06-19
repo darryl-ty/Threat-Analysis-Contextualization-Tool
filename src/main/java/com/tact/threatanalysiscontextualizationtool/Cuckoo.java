5fff43b1e0ed666b50c535edc5635ab44344eb63
darryl-ty <darrylterrell921@gmail.com>
2023-06-19 14:53:55 -0400

    Start implementation of multithreaded file classes to gather OSINT info.

---
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/Cuckoo.java
@@ -0,0 +1,9 @@
+package com.tact.threatanalysiscontextualizationtool;
+
+public class Cuckoo extends Thread{
+
+    private int threatRating;
+    public void run(){
+
+    }
+}
---
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/VirusTotalFile.java
@@ -0,0 +1,9 @@
+package com.tact.threatanalysiscontextualizationtool;
+
+public class VirusTotalFile extends Thread {
+
+    private int threatRating;
+    public void run(){
+
+    }
+}

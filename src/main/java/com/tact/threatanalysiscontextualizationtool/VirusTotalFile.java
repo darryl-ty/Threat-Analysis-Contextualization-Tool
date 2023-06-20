e71be0a48ac8627658f3838fea48bc72eb4dff7b
darryl-ty <darrylterrell921@gmail.com>
2023-06-19 23:53:23 -0400

    Include website driving library.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/VirusTotalFile.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/VirusTotalFile.java
@@ -1,9 +1,19 @@
 package com.tact.threatanalysiscontextualizationtool;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class VirusTotalFile extends Thread {
 
     private int threatRating;
-    public void run(){
 
+    public VirusTotalFile(String sourceFile){
+
+    }
+    public void run(){
+        Document doc = Jsoup.parse("https://www.virustotal.com/gui/file/43d54b8b86ae2493574243c900db4aef7fc5dd96ea5188ceaafecda01c02130b/details");
+        System.out.println(doc.title());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.tact.threatanalysiscontextualizationtool {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.jsoup;
+    requires org.seleniumhq.selenium.chrome_driver;
 
     opens com.tact.threatanalysiscontextualizationtool to javafx.fxml;
     exports com.tact.threatanalysiscontextualizationtool;

b2c4cd2f925ff4cb04fcd14d28de67903eb0a6ec
darryl-ty <darrylterrell921@gmail.com>
2023-07-05 22:09:12 -0400

    Add getters for URL record.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/URL.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/URL.java
@@ -2,4 +2,18 @@ package com.tact.threatanalysiscontextualizationtool;
 
 public record URL(Talos talos, URLVoid urlVoid, VirusTotalURL vtURL) {
 
+    @Override
+    public Talos talos() {
+        return talos;
+    }
+
+    @Override
+    public URLVoid urlVoid() {
+        return urlVoid;
+    }
+
+    @Override
+    public VirusTotalURL vtURL() {
+        return vtURL;
+    }
 }

87760b0d0463a93a189161a764e7f083eaaa5379
darryl-ty <darrylterrell921@gmail.com>
2023-07-06 12:35:50 -0400

    Remove irrelevant import.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/URLVoid.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/URLVoid.java
@@ -1,6 +1,5 @@
 package com.tact.threatanalysiscontextualizationtool;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;

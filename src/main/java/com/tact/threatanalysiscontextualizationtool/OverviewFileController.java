958e96daa44ffa63469f6897f1342c5ab0c22599
darryl-ty <darrylterrell921@gmail.com>
2023-07-11 00:21:50 -0500

    Add advancedFileInfo method to utilize advanced vt info.

--- src/main/java/com/tact/threatanalysiscontextualizationtool/OverviewFileController.java
+++ src/main/java/com/tact/threatanalysiscontextualizationtool/OverviewFileController.java
@@ -7,12 +7,36 @@ public class OverviewFileController {
     private FILE overview;
     @FXML
     private Label fileOverviewName;
+    @FXML
+    private Label fileType;
+    @FXML
+    private Label fileSize;
+    @FXML
+    private Label fileHash;
+
+
     public OverviewFileController(FILE overview){
         this.overview = overview;
     }
 
     @FXML
     public void initialize(){
+        fieldPopulation();
+    }
+
+    private void fieldPopulation() {
+        fileBasicInfo();
+        fileAdvancedInfo();
+    }
+
+    private void fileBasicInfo() {
         fileOverviewName.setText(overview.vtFile().getFileName());
+        fileType.setText(overview.vtFile().getFileType());
+        fileSize.setText(String.valueOf(overview.vtFile().getFileSize()) + " Bytes");
+        fileHash.setText(overview.vtFile().getHashValue());
+    }
+
+    private void fileAdvancedInfo(){
+
     }
 }
